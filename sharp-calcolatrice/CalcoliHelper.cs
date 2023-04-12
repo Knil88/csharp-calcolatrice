7a2db7cd0f7f5fafb06a31c38cac2602479d3cdc
Yari <narducciyarigmail.com>
2023-04-12 16:56:44 +0200

    Aggiungere i file di progetto.

---
+++ sharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_calcolatrice
+{
+    public static class CalcoliHelper
+    {
+        public static int Somma(int a, int b) => a + b;
+        public static double Somma(double a, double b) => a + b;
+        public static int Differenza(int a, int b) => a - b;
+        public static double Differenza(double a, double b) => a - b;
+        public static int Moltiplicazione(int a, int b) => a * b;
+        public static double Moltiplicazione(double a, double b) => a * b;
+        public static int ValoreAssoluto(int n) => Math.Abs(n);
+        public static double ValoreAssoluto(double n) => Math.Abs(n);
+        public static int Minimo(int a, int b) => Math.Min(a, b);
+        public static double Minimo(double a, double b) => Math.Min(a, b);
+        public static int Massimo(int a, int b) => Math.Max(a, b);
+        public static double Massimo(double a, double b) => Math.Max(a, b);
+
+
+
+    }
+}
---
+++ sharp-calcolatrice/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace sharp_calcolatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalcoliHelper.Somma(1, 2));
+            Console.WriteLine(CalcoliHelper.Somma(1.0, 2.0));
+            Console.WriteLine(CalcoliHelper.Differenza(1, 2));
+            Console.WriteLine(CalcoliHelper.Differenza(1.0, 2.0));
+            Console.WriteLine(CalcoliHelper.Moltiplicazione(1, 2));
+            Console.WriteLine(CalcoliHelper.Moltiplicazione(1.0, 2.0));
+            Console.WriteLine(CalcoliHelper.ValoreAssoluto(-1));
+            Console.WriteLine(CalcoliHelper.ValoreAssoluto(-1.0));
+            Console.WriteLine(CalcoliHelper.Minimo(1, 2));
+            Console.WriteLine(CalcoliHelper.Minimo(1.0, 2.0));
+            Console.WriteLine(CalcoliHelper.Massimo(1, 2));
+            Console.WriteLine(CalcoliHelper.Massimo(1.0, 2.0));
+        }
+      
+
+    }
+
+}
